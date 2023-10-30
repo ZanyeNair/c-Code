88ecd44dca21b2f6fbd6f31b55587a414ec3bf5b
ZanyeNair <zayne.nair@circuitrunners.com>
2023-10-30 15:59:13 -0400

    quick

--- nc.cpp
+++ nc.cpp
@@ -1,6 +1,13 @@
-using namespace std;
-#include <iostream>
-int main(){
-    cout << "Hello World";
-    return 0;
-}
+using namespace std;//we can actually use names
+#include <iostream>//This is just importing the file from the library
+int main(){//this is a function shockers
+    cout << "Hello World!" << endl;//this is a print statement
+    int val = 2;//declaring a new variable
+    val = val+2;
+    int tval = val/2;
+    int val1 = 1, val2 = 2, val3 = 3;
+    cout << val + val1+ val2 + val3;//printing the variable
+    const double pi = 3.14159265358979323846;
+
+    return 0; // ends the method
+}   
