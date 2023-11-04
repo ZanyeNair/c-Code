33219de7e496d595daafe11340f31845d8e23ce1
ZanyeNair <zayne.nair@circuitrunners.com>
2023-11-03 20:47:20 -0400

    almost done w error

--- wordsort.cpp
+++ wordsort.cpp
@@ -5,22 +5,24 @@ using namespace std;
 int main() {
     string word;
     char letter;
+    int j = 0;
+    
     bool equ = true;
     char alphabet[26] = {'a','b','c', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n','o','p','q','r','s','t','u','v','w','x','y','z'};
     cout << "Word to type: "<< endl;
     cin >> word;
+    char wlist[word.length()-1] = {};
     for(int i = 0; i < word.length(); i++){
         letter = word[i];
-        for(int j = 0; j < 26; j++){
-            equ= true;
-            while(equ){
-                
-                cout << letter + "=" + alphabet[j] << endl;
-                if(letter == alphabet[j]){
-                cout << alphabet[j] << endl;
+        equ= true;
+        j = 0;
+        while(equ && j < 26){
+            
+            cout << alphabet[j] << endl;
+            if(letter == alphabet[j]){
                 equ = false;
             }
-            }
+            j++;
         }
     }
     return 0;
